04a7bd77267926f6f885db747310bc56fd7cf017
Oliwier-Gebczynski <65087108+Oliwier-Gebczynski@users.noreply.github.com>
2020-11-11 21:34:15 +0100

    login system for one person

--- Login/app.py
+++ Login/app.py
@@ -2,13 +2,23 @@ import pickle
 
 print("Hello in basic login system!")
 
-login_input = {1: "oliwier", 2: "qwerty"}
+#login_input = {1: "oliwier", 2: "qwerty"}
 
-data_out = open("database.txt", "wb")
-pickle.dump(login_input, data_out)
-data_out.close()
+#data_out = open("database.txt", "wb")
+#pickle.dump(login_input, data_out)
+# data_out.close()
 
 data_in = open("database.txt", "rb")
 login_data = pickle.load(data_in)
+data_in.close()
 
-print(login_data[2])
+login = input("Login: ")
+
+if login_data[1] == login:
+    password = input("Password: ")
+    if login_data[2] == password:
+        print("Succes!")
+    else:
+        print("Incorrect password")
+else:
+    print("Incorrect login")
