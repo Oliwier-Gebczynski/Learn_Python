531a5984c4eea30b2704787bac88f4ae9adc4ea9
Oliwier-Gebczynski <65087108+Oliwier-Gebczynski@users.noreply.github.com>
2020-11-11 21:27:25 +0100

    read data from txt with pickle

--- Login/app.py
+++ Login/app.py
@@ -7,3 +7,8 @@ login_input = {1: "oliwier", 2: "qwerty"}
 data_out = open("database.txt", "wb")
 pickle.dump(login_input, data_out)
 data_out.close()
+
+data_in = open("database.txt", "rb")
+login_data = pickle.load(data_in)
+
+print(login_data[2])
