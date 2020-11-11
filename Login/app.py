22ba1331b5a6775a17489a9b59f10fe1789f269c
Oliwier-Gebczynski <65087108+Oliwier-Gebczynski@users.noreply.github.com>
2020-11-11 14:49:28 +0100

    password section to correct

--- Login/app.py
+++ Login/app.py
@@ -1,5 +1,13 @@
 print("Hello in basic login system!")
 
 login_data = open("database.txt", "r")
+login = input("Login: ")
 
-print(login_data)
+if login == login_data.readlines()[0].split()[0]:
+    password = input("Password: ")
+    print(password)
+
+    if password == login_data.readlines()[0].split()[-1]:
+        print("Succesfull!")
+else:
+    print("Its wrong login!")
