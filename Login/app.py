ded2ec427e40b0608cc99d7809670e012f2043ca
Oliwier-Gebczynski <65087108+Oliwier-Gebczynski@users.noreply.github.com>
2020-11-11 22:41:41 +0100

    finished login system

--- Login/app.py
+++ Login/app.py
@@ -2,24 +2,31 @@ import pickle
 
 print("Hello in basic login system!")
 
-login_input = {
-    1: ["oliwier", "karol", "kuba"],
-    2: ["qwerty", "haslo", "maslo"]
-}
+# login dict
+# login_input = {
+#  1: ["oliwier", "karol", "kuba"],
+#  2: ["qwerty", "haslo", "maslo"]
+# }
 
-data_out = open("database.txt", "wb")
-pickle.dump(login_input, data_out)
-data_out.close()
+# add new users
+#data_out = open("database.txt", "wb")
+#pickle.dump(login_input, data_out)
+# data_out.close()
 
+# read data from .txt
 data_in = open("database.txt", "rb")
 login_data = pickle.load(data_in)
 data_in.close()
 
+# input login, get login index
 login = input("Login: ")
+login_user = login_data[1]
+x = login_user.index(login)
 
-if login_data[1][2] == login:
+# search login in database, "x" is the login index of array, "login and password" pairs have the same index
+if login_data[1][x] == login:
     password = input("Password: ")
-    if login_data[2][2] == password:
+    if login_data[2][x] == password:
         print("Succes!")
     else:
         print("Incorrect password")
