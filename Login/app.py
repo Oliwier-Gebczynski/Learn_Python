e510385fd8bd3c25588f4d4583b0a060a8ff067d
Oliwier-Gebczynski <65087108+Oliwier-Gebczynski@users.noreply.github.com>
2020-11-11 22:19:23 +0100

    login with many accounts ( not automaticly, manual index )

--- Login/app.py
+++ Login/app.py
@@ -2,11 +2,14 @@ import pickle
 
 print("Hello in basic login system!")
 
-#login_input = {1: "oliwier", 2: "qwerty"}
+login_input = {
+    1: ["oliwier", "karol", "kuba"],
+    2: ["qwerty", "haslo", "maslo"]
+}
 
-#data_out = open("database.txt", "wb")
-#pickle.dump(login_input, data_out)
-# data_out.close()
+data_out = open("database.txt", "wb")
+pickle.dump(login_input, data_out)
+data_out.close()
 
 data_in = open("database.txt", "rb")
 login_data = pickle.load(data_in)
@@ -14,9 +17,9 @@ data_in.close()
 
 login = input("Login: ")
 
-if login_data[1] == login:
+if login_data[1][2] == login:
     password = input("Password: ")
-    if login_data[2] == password:
+    if login_data[2][2] == password:
         print("Succes!")
     else:
         print("Incorrect password")
