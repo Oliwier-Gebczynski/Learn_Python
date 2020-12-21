28594d9aae2fcf345074410d5b8f60f979bb7c79
Oliwier-Gebczynski <65087108+Oliwier-Gebczynski@users.noreply.github.com>
2020-12-21 19:59:32 +0100

    template and signs

---
+++ PasswordGenerator/app.py
@@ -0,0 +1,13 @@
+big_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+               "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+small_letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+                 "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+special_signs = ["!", "@", "#", "$", "%", "&", "*", "?"]
+
+numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+
+def input_data():
+    how_many = input("How long the password must be: ")
