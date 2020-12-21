5980bace5403386a1284ddbad47959287329c735
Oliwier-Gebczynski <65087108+Oliwier-Gebczynski@users.noreply.github.com>
2020-12-21 21:59:43 +0100

    done password generator

--- PasswordGenerator/app.py
+++ PasswordGenerator/app.py
@@ -1,3 +1,5 @@
+import random
+
 big_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
                "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
@@ -8,12 +10,34 @@ special_signs = ["!", "@", "#", "$", "%", "&", "*", "?"]
 
 numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-how_many = input("How long the password must be (max = 30): ")
+
+def password_generator(how_many):
+    password = []
+    password.append(random.choice(big_letters))
+    password.append(random.choice(small_letters))
+    password.append(random.choice(special_signs))
+    password.append(random.choice(numbers))
+
+    all_signs = []
+    all_signs.extend(big_letters)
+    all_signs.extend(small_letters)
+    all_signs.extend(special_signs)
+    all_signs.extend(numbers)
+
+    while len(password) < how_many:
+        password.append(random.choice(all_signs))
+
+    random.shuffle(password)
+
+    print(password)
+
+
+how_many = input("How long the password must be (max = 30, min = 4): ")
 how_many = int(how_many)
 
 if how_many > 30:
     print("To many characters!")
-elif how_many <= 0:
+elif how_many < 4:
     print("You didnt specify the lenght!")
 else:
-    print(how_many)
+    password_generator(how_many)
