a471eccea3ba4c0ce89669334485a3712571a0d9
Oliwier-Gebczynski <65087108+Oliwier-Gebczynski@users.noreply.github.com>
2021-01-14 16:31:34 +0100

    add input and grid position

--- passBase/main.py
+++ passBase/main.py
@@ -8,7 +8,25 @@ window.title("passBase")
 # add small logo
 window.iconbitmap('source\ico\key.ico')
 
-# add photo
+# add text
+helloText = Label(window, text="Hello in passBase!", bg="black", fg="white")
+helloText.grid(row=0, column=1)
 
+loginText = Label(window, text="Login: ", bg="black", fg="white")
+loginText.grid(row=1, column=0)
+
+passwordText = Label(window, text="Password: ", bg="black", fg="white")
+passwordText.grid(row=2, column=0)
+
+# inputs
+loginInput = Entry(window)
+loginInput.grid(row=1, column=2)
+
+passwordInput = Entry(window)
+passwordInput.grid(row=2, column=2)
+
+# add button
+submitBtn = Button(window, text="Submit")
+submitBtn.grid(row=3, column=2)
 
 window.mainloop()
