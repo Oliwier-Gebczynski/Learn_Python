5474a31288293904840332a9edce055555ffb152
Oliwier-Gebczynski <65087108+Oliwier-Gebczynski@users.noreply.github.com>
2021-12-17 20:18:26 +0100

    html done

--- CryptoMail/app.py
+++ CryptoMail/app.py
@@ -1,6 +1,5 @@
 # TODO
-# - stworzenie pliku html z danymi
-# - dodanie zdjec (logo) do kryptowalut
+# - stworzenie pliku html z danymi // ZROBIONE
 # - zapoznanie sie z wysyłaniem email przez python
 # - zrobienie listy email
 # - wysyłanie email za każdym otworzeniem programu
@@ -8,6 +7,7 @@
 import requests
 import json
 import apikey
+import datetime
 
 def get_data():
     result_list = []
@@ -29,8 +29,35 @@ def get_data():
     coins = json['data']
 
     for coin in coins:
-        result = f"{coin['name']} ({coin['symbol']}): Price {coin['quote']['PLN']['price']} pln, Change in 24 hours: {coin['quote']['PLN']['percent_change_24h']}%"
+        result = f"{coin['name']} ({coin['symbol']}):    Cena {coin['quote']['PLN']['price']} pln,    Zmiana przez 24 godziny: {coin['quote']['PLN']['percent_change_24h']}%"
         result_list.append(result)
 
     return result_list
+
+def create_html(list):
+    time = datetime.datetime.now()
+    # _________________HEAD___________________
+    html_head = f'<head>  <meta charset="UTF-8"> <link rel="stylesheet" href="style.css"> </head>'
+    # ________________BODY___________________
+    html_body = '<body>' # otwarcie znacznika body
+    html_logo = f'<h1 class="logo"> ogCode cryptoStats </h1>' # stworzenie logo/nazwy
+    html_title = f'<h2 class="title"> Cena kryptowalut z {time.strftime("%x")} </h2>' # stworzenie tytułu
+    html_ul = f'<ul class="list">' # lista kryptowalut
+    for item in list:
+        html_li = f'<li class="list_element"> {item} </li>' # stworzenie elementu li
+        html_ul += html_li # dodanie elementu do ul
+    html_ul += f'</ul>' # domkniecie ul
+    html_link = f'<a class="link" href="https://github.com/Oliwier-Gebczynski"> ogCode GitHub </a>'
+    html_body += html_logo + html_title + html_ul + html_link + '</body>'
+    # ________________RESULT__________________
+    html = html_head + html_body
+    # ________________SAVE FILE_______________
+    file = open("html/index.html", "w")
+    file.write(html)
+    file.close()
+
+create_html(get_data())
+
+
+
 
