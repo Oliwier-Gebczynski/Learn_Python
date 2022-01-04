385a5f3ef3f0bcb665a2028fee52ce9579a0cda3
Oliwier-Gebczynski <65087108+Oliwier-Gebczynski@users.noreply.github.com>
2022-01-04 09:34:49 +0100

    done

--- Codewars/Roman Numerals Helper.py
+++ Codewars/Roman Numerals Helper.py
@@ -1,47 +1,25 @@
 def to_roman(val):
     result = ""
+    value = [
+        1000, 900, 500, 400,
+        100, 90, 50, 40,
+        10, 9, 5, 4,
+        1
+    ]
+    symbol = [
+        "M", "CM", "D", "CD",
+        "C", "XC", "L", "XL",
+        "X", "IX", "V", "IV",
+        "I"
+    ]
+
+    i = 0
     while val > 0:
-        while val >= 1000:
-            result += "M"
-            val -= 1000
-
-        while val >= 500:
-            result += "D"
-            val -= 500
-
-        while val >= 100:
-            result += "C"
-            val -= 100
-
-        while val >= 50:
-            result += "L"
-            val -= 50
-
-        while val >= 10:
-            result += "X"
-            val -= 10
-
-        if val == 9:
-            result += "IX"
-        elif val == 8:
-            result += "VIII"
-        elif val == 7:
-            result += "VII"
-        elif val == 6:
-            result += "VI"
-        elif val == 5:
-            result += "V"
-        elif val == 4:
-            result += "IV"
-        elif val == 3:
-            result += "III"
-        elif val == 2:
-            result += "II"
-        elif val == 1:
-            result += "I"
-
-        val = 0
-        return result
+        for _ in range(val // value[i]):
+            result += symbol[i]
+            val -= value[i]
+        i += 1
+    return result
 
 
 def from_roman(roman_num):
@@ -59,12 +37,15 @@ def from_roman(roman_num):
     for index in range(len(roman_num) - 1):
         first = roman_num[index]
         second = roman_num[index + 1]
+        print(first)
+        print(second)
 
-        if sign_map[first] < sign_map[second]: # MCMXC
-            result -= sign_map[first]  # 1. -, 2. -, 3.
+        if sign_map[first] < sign_map[second]:
+            result -= sign_map[first]
         else:
-            result += sign_map[first]  # 1. 1000, 2. 1500,
+            result += sign_map[first]
         print(result)
+        print("____________________________")
 
     result += sign_map[roman_num[-1]]
     return result
