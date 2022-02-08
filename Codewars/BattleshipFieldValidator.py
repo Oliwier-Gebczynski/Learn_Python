4f39427ec43584eff82500edc1f44f8e1479e60c
Oliwier-Gebczynski <65087108+Oliwier-Gebczynski@users.noreply.github.com>
2022-02-08 18:31:13 +0100

    next step

--- Codewars/BattleshipFieldValidator.py
+++ Codewars/BattleshipFieldValidator.py
@@ -1,13 +1,13 @@
 battleField = [[1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
-                 [1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
-                 [1, 0, 1, 0, 1, 1, 1, 0, 1, 0],
-                 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
-                 [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-                 [0, 0, 0, 1, 0, 0, 0, 1, 0, 0],
-                 [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+[1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+[1, 0, 1, 0, 1, 1, 1, 0, 1, 0],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+[0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+[0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
 def validate_battlefield(field):
     shipCell = 0
@@ -17,18 +17,30 @@ def validate_battlefield(field):
                 shipCell += 1
 
     if shipCell == 20:
-        check_cell(field)
-
-    print(shipCell)
-
-# jezeli jest =1 sprawdz do okoła czy sa jedynki. Jezeli jest 1 w dół to sprawdzaj tylko kolumne w dół do =0 / przy okazji sprawdz wszystkie komorki czy nie maja po bokach innych jedynek, jesli tak to wyswietl false
+        if check_cell(field) == False:
+            return False
+        elif check_cell(field) == None:
+            return True
+    else:
+        return False
 
 def check_cell(field):
-    for i in range(len(str(field))-1):
-        for j in range(len(str(field))-1):
+    for i in range(9):
+        for j in range(9):
             if ((1 <= j <= 8) and (1 <= i <= 8)) and field[i][j] == 1:
                 if field[i+1][j+1] == 1 or field[i+1][j-1] == 1 or field[i-1][j+1] == 1 or field[i-1][j-1] == 1:
                     return False
+
+def check_ships(field):
+    result = []
+    for i in range(9):
+        list = []
+        for j in range(9):
+            if field[i][j] == 1:
+                list
+
+
+
 
 
 
