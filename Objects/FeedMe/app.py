abe599b99b166eedf9ec53a5ca8a8772d0b8b98f
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2022-06-13 17:36:30 +0200

    Next step

--- Objects/FeedMe/app.py
+++ Objects/FeedMe/app.py
@@ -1,6 +1,4 @@
 from datetime import date
-from unicodedata import name
-
 
 cat = """
 |\__/,|   (`)
@@ -9,6 +7,12 @@ cat = """
 (((^_(((/(((_/
 """
 
+kitties = []
+doggos = []
+
+kind = "dog"
+
+# pets calss
 class Cat:
     def __init__(self, name, age, color, date):
         self.name = name
@@ -16,7 +20,44 @@ class Cat:
         self.color = color
         self.date = date
 
-newKitty = Cat("Kinny", 1, "black", "10.11.2022")
+class Dog:
+    def __init__(self, name, age, color, today):
+        self.name = name
+        self.age = age
+        self.color = color
+        self.date = date
+
+#functions
+def newPuppy(name, age, color, today):
+    if kind == "DOG":
+        doggo = Dog(name, age, color, today)
+        doggos.append(doggo)
+    elif kind == "CAT":
+        kitty = Cat(name, age, color, today)
+        kitties.append(kitty)
+    elif (kind != "DOG") or (kind != "CAT"):
+        print("There is no such type of pet")
+
+    print(doggos, kitties)
+
+def main():
+    kind = input("Type kind of pet(dog or cat): ").upper()
+    
+    if (kind == "DOG") or (kind == "CAT"):
+        name = input("Name: ")
+        age = input("Age: ")
+        color = input("Color: ")
+        today = date.today()
+
+        newPuppy(name, age, color, today)
+
+    elif (kind != "DOG") or (kind != "CAT"):
+        main()
+
+main()
+
+for item in kitties:
+    print(f"{item.name}, {item.age}, {item.colour}, {item.date}")
 
 
 
