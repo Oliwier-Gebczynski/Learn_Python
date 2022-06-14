db55b8e1f6902035c2a0cc496bbcefd0c7eeb6e5
Oliwier-Gebczynski <65087108+Oliwier-Gebczynski@users.noreply.github.com>
2022-06-14 12:49:40 +0200

    update

--- Objects/FeedMe/app.py
+++ Objects/FeedMe/app.py
@@ -1,9 +1,6 @@
 from datetime import date
-<<<<<<< HEAD
 import pandas as pd
 
-=======
->>>>>>> 1c65aa77215a59cb6b4e024f6ede5c6ec9a72cbd
 #start with pandas
 
 cat = """
@@ -12,10 +9,6 @@ cat = """
 ( T   )     /
 (((^_(((/(((_/
 """
-
-kitties = []
-doggos = []
-
 # pets calss
 class Cat:
     def __init__(self, name, age, color, date, kind):
@@ -30,11 +23,7 @@ class Cat:
         return new_row
 
 class Dog:
-<<<<<<< HEAD
     def __init__(self, name, age, color, date, kind):
-=======
-    def __init__(self, name, age, color, date):
->>>>>>> 1c65aa77215a59cb6b4e024f6ede5c6ec9a72cbd
         self.name = name
         self.age = age
         self.color = color
@@ -46,11 +35,7 @@ class Dog:
         return new_row
 
 #functions
-<<<<<<< HEAD
 def newPuppy(name, age, color, today, kind, df):
-=======
-def newPuppy(name, age, color, today, kind):
->>>>>>> 1c65aa77215a59cb6b4e024f6ede5c6ec9a72cbd
     if kind == "DOG":
         doggo = Dog(name, age, color, today, kind)
         database_doggo = doggo.addToDataframe()
@@ -63,12 +48,10 @@ def newPuppy(name, age, color, today, kind):
         return file
     elif (kind != "DOG") or (kind != "CAT"):
         print("There is no such type of pet")
-
-
+        
 def openFile():
     df = pd.read_csv('database.csv')
     return df
-
 
 def main():
     file = openFile()
@@ -80,15 +63,11 @@ def main():
         color = input("Color: ")
         today = date.today()
 
-<<<<<<< HEAD
         file = newPuppy(name, age, color, today, kind, file)
 
         file.to_csv("database.csv", index=False)
 
         print(file)
-=======
-        newPuppy(name, age, color, today, kind)
->>>>>>> 1c65aa77215a59cb6b4e024f6ede5c6ec9a72cbd
 
     elif (kind != "DOG") or (kind != "CAT"):
         main()
@@ -97,11 +76,6 @@ def main():
 
 main()
 
-<<<<<<< HEAD
-=======
-for item in kitties:
-    print(f"{item.name}, {item.age}, {item.color}, {item.date}")
->>>>>>> 1c65aa77215a59cb6b4e024f6ede5c6ec9a72cbd
 
 
 
